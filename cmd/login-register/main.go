3f86d1e73ebf97dbb6ae763aa14f9b8d76299374
roiciap <roizab2000@gmail.com>
2024-02-22 01:43:08 +0100

    use env variable for jwt secret

--- cmd/login-register/main.go
+++ cmd/login-register/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/roiciap/golang/internal/http/handlers"
 	myauth "github.com/roiciap/golang/pkg/auth"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	authHandler := myauth.CreateJWTAuthentication([]byte("secret"))
+func initHttpHandler() (http.Handler, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	if jwtSecret == "" {
+		return nil, errors.New("aha")
+	}
+
+	authHandler := myauth.CreateJWTAuthentication([]byte(jwtSecret))
 	handler, err := handlers.CreateAccountHandler(authHandler)
 	if err != nil {
 		fmt.Println(err.Error())
+	}
+	return handler, nil
+}
+
+func main() {
+	mux := http.NewServeMux()
+	handler, err := initHttpHandler()
+
+	if err != nil {
+		log.Fatal("No JWT_SECRET provided")
+		os.Exit(1)
 	}
 
 	mux.Handle("/login", handler)
